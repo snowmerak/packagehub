55014d23f2f031796a8e05e7620decb1d0609e83
snowmerak <snowmerak@outlook.com>
2022-06-09 00:39:14 +0900

    add AddPath

--- windows/setx.go
+++ windows/setx.go
@@ -9,14 +9,21 @@ import (
 func SetX(key, value string) error {
 	cmd := exec.Command("setx", key, value)
 	cmd.Stdin = os.Stdin
-	result, err := cmd.Output()
-	if err != nil {
-		return fmt.Errorf("SetX: %w", err)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("SetX: cmd.Start: %w", err)
 	}
-	fmt.Println(string(result))
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("SetX: cmd.Wait: %w", err)
+	}
 	return nil
 }
 
 func GetEnv(key string) string {
 	return os.Getenv(key)
+}
+
+func AddPath(path string) error {
+	return SetX("Path", GetEnv("Path")+";"+path)
 }
